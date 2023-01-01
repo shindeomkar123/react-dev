acc835eaebc64fd5e5c840d3dcb6bbd173a5384f
Omkar Laxman Shinde <omkar.shinde@yash.com>
2023-01-01 16:38:30 +0530

    Ignite your app using parcel

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading1 = React.createElement("h1", { id: "heading1" }, "Heading 1");
+// const heading2 = React.createElement("h1", { id: "heading2" }, "Heading 2");
+
+//! React element which is object
+// const header = React.createElement("div", { className: "header" }, [
+//   heading1,
+//   heading2,
+// ]);
+
+const title = <h1>Namaste React </h1>;
+const Title = () => {
+  return <h1>Namaste React</h1>;
+};
+
+// Functional component which returns JSX
+const Header = () => {
+  return (
+    <div>
+      <Title />
+      <h1>Heading 1</h1>
+      <h1>Heading 2</h1>
+    </div>
+  );
+};
+
 //! Root where react app renderes
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const heading1 = React.createElement("h1", { id: "heading1" }, "Heading 1");
-const heading2 = React.createElement("h1", { id: "heading2" }, "Heading 2");
-
-//! React element which is object
-const header = React.createElement("div", { className: "header" }, [
-  heading1,
-  heading2,
-]);
-
-root.render(header);
+root.render(<Header />);
